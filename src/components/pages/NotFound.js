6b6a207443542eec248e638fd8980139b5e2dc54
rhallgarth88 <59847530+rhallgarth88@users.noreply.github.com>
2021-06-29 15:37:19 -0400

    Added NotFound component

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div class="all-center">
+            <h1>Not Found</h1>
+            <p className="lead">The page you're looking for doesn't exist.</p>
+        </div>
+    )
+}
+
+export default NotFound;
